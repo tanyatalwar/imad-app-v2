3fbb95332eb0c5f6b913c0810990ccf113a2aea6
tanyatalwar <tanyatalwar8@gmail.com>
2017-02-16 23:19:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,10 @@ element.innerHTML = 'New value';
 
 //to move the image
 var img = document.getElementById('modi');
-//using onclick event
+//using onclick event shift to left
 img.onclick = function(){
-    modi.style.marginLeft='100px';
+    //giving jumping effect through css 
+    // modi.style.marginLeft='100px';
+    //giving moving effect through setInterval
+var interval = setInterval(moveLeft, 100)  ;  
 };
