bc462d13ce4aa47202f4d4022487fbdcdf5203fa
tanyatalwar <tanyatalwar8@gmail.com>
2017-02-16 23:06:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,6 @@
 console.log('Loaded!');
 //change the text of the main dev
+//getting the particular element from the html element
 var element = document.getElementById('main.txt');
+//changing the inner html content to new
 element.innerHTML = 'New value';
