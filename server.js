46338b982d9fcc92713b70834e100ee7464771cc
tanyatalwar <tanyatalwar8@gmail.com>
2017-02-16 21:24:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function createTemplate(data)
 var title = data.title;
 var heading = data.heading;
 var date = data.date;
-var content = content.data;
+var content = data.content;
 
 var htmlTemplate =`
 <html>
