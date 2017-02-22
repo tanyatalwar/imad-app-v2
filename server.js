85e97d46457d966a386a6fce6ae16b62223afacb
tanyatalwar <tanyatalwar8@gmail.com>
2017-02-22 23:33:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'tanyatalwar',
+    database: 'tanyatalwar',
+    host: 'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -61,10 +70,19 @@ var htmlTemplate =`
 </html>`;
 return createTemplate;
 }
-
+var pool = new Pool(config);
 app.get('/test-db', function(req,res){
     //make a select request
     //return the response with the result
+    pool.query('SELECT * FROM test',function(err, result){
+      if(err){
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send(JSON.stringify(result));
+      }
+    });
 });
 
 
