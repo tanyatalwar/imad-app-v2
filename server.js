ed55c5aa6db55f0f33a5b032f47275fc10817b6b
tanyatalwar <tanyatalwar8@gmail.com>
2017-02-22 23:44:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/test-db', function(req,res){
       }
       else
       {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
     });
 });
